f5aa5e0d42b8ccf66fbf07632fd7acb55fcaf5eb
OmarDev <201400127@gift.edu.pk>
2024-09-05 00:32:58 +0500

    Laptop Model Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { Canvas } from "@react-three/fiber";
 import React from "react";
+import "./styles.css";
+import { Environment, OrbitControls, ScrollControls } from "@react-three/drei";
+import MacContainer from "./MacContainer";
 
 function App() {
-  return <div>Appp</div>;
+  return (
+    <Canvas camera={{ fov: 12, position: [0, -2, 220] }}>
+      <Environment
+        files={[
+          "https://dl.polyhaven.org/file/ph-assets/HDRIs/exr/4k/studio_small_09_4k.exr",
+        ]}
+      />
+      <ScrollControls>
+        <MacContainer />
+      </ScrollControls>
+    </Canvas>
+  );
 }
 
 export default App;
---
+++ src/MacContainer.jsx
@@ -0,0 +1,21 @@
+import { useGLTF } from "@react-three/drei";
+import React from "react";
+import * as THREE from "three";
+
+function MacContainer() {
+  let model = useGLTF("./mac.glb");
+  let meshes = {};
+  model.scene.traverse((e) => {
+    meshes[e.name] = e;
+  });
+
+  meshes.screen.rotation.x = THREE.MathUtils.degToRad(-180);
+
+  return (
+    <group position={[0, -10, 20]}>
+      <primitive object={model.scene} />
+    </group>
+  );
+}
+
+export default MacContainer;
